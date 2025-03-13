66a8b073fdadf0f2aea06a397b39fe3448cd692c
Hedi Rihani <hedi.rihani10@gmail.com>
2025-03-13 09:55:53 +0100

    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-const burgerMenu = document.getElementById('burgerMenu');
-const menu = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
